9fb3054427ca517e6f814b30f2e23eff9786ea67
Rufian55 <chriskearns@rigidbody.tech>
2021-08-01 06:31:28 -0500

    Refreshed!

--- bubbleSort.c
+++ bubbleSort.c
@@ -9,40 +9,40 @@
 #include <time.h>
 
 int main() {
-	int anArray[100], n, c, d, swap;
+    int anArray[100], n, c, d, swap;
 
-	/** Alternate random anArray initialization **/
-	// int i, len;
-	// len = sizeof(anArray) / sizeof(int);
-	// srand(time(NULL));
+    /** Alternate random anArray initialization **/
+    // int i, len;
+    // len = sizeof(anArray) / sizeof(int);
+    // srand(time(NULL));
 
-	// for (i = 0; i < len; i++) {
-	//	 anArray[i] = rand();
-	// }
+    // for (i = 0; i < len; i++) {
+    //	 anArray[i] = rand();
+    // }
 
-	printf("Enter number of elements\n");
-	scanf("%d", &n);
+    printf("Enter number of elements\n");
+    scanf("%d", &n);
 
-	printf("Enter %d integers\n", n);
+    printf("Enter %d integers\n", n);
 
-	for (c = 0; c < n; c++)
-		scanf("%d", &anArray[c]);
+    for (c = 0; c < n; c++)
+        scanf("%d", &anArray[c]);
 
-	for (c = 0; c < (n - 1); c++) {
-		for (d = 0; d < n - c - 1; d++) {
-			// For decreasing order use < .
-			if (anArray[d] > anArray[d + 1]) {
-				swap = anArray[d];
-				anArray[d] = anArray[d + 1];
-				anArray[d + 1] = swap;
-			}
-		}
-	}
+    for (c = 0; c < (n - 1); c++) {
+        for (d = 0; d < n - c - 1; d++) {
+            // For decreasing order use < .
+            if (anArray[d] > anArray[d + 1]) {
+                swap = anArray[d];
+                anArray[d] = anArray[d + 1];
+                anArray[d + 1] = swap;
+            }
+        }
+    }
 
-	printf("Sorted list in ascending order:\n");
+    printf("Sorted list in ascending order:\n");
 
-	for (c = 0; c < n; c++)
-		printf("%d\n", anArray[c]);
+    for (c = 0; c < n; c++)
+        printf("%d\n", anArray[c]);
 
-	return 0;
+    return 0;
 }
