ab51278dd9496a153505187aef8bfa8a9ab3b69e
Rufian55 <chriskearns@rigidbody.tech>
2021-08-19 01:50:42 -0500

    Updated for git

--- InsertionSort.c
+++ InsertionSort.c
@@ -6,46 +6,49 @@
 #include <stdlib.h>
 
 int main() {
-	int i, j, temp, userInput = -1;
-	int *arr;
+    int i, j, temp, userInput = -1;
+    int *arr;
 
-	while(userInput < 0){
+	while (userInput < 0) {
 		printf("Enter total elements to be sorted: ");
 		scanf("%d", &userInput);
-		if (userInput < 1)
-			printf("Sorry, input must be > 0.\n");
-	}
+        if (userInput < 1) {
+            printf("Sorry, input must be > 0.\n");
+        }
+    }
 
-	arr = malloc(userInput * sizeof(int));
+    arr = malloc(userInput * sizeof(int));
 
-	if (arr == NULL) {
-		printf("Memory allocation error!\n");
-		exit(1);
-	}
+    if (arr == NULL) {
+        printf("Memory allocation error!\n");
+        exit(1);
+    }
 
-	printf("Seperated by spaces or <Enter>,\nPlease enter %d elements: ", userInput);
-	for (i = 0; i < userInput; i++)
-		scanf("%d", &arr[i]);
+    printf("Seperated by spaces or <Enter>,\nPlease enter %d elements: ", userInput);
+    for (i = 0; i < userInput; i++) {
+        scanf("%d", &arr[i]);
+    }
 
-	// Starting with 2nd element of arr[]...
-	for (i = 1; i < userInput; i++) {
-		temp = arr[i];
-		j = i - 1;
-		while ((temp < arr[j]) && (j >= 0)) {
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
-		arr[j + 1] = temp;
-	}
+    // Starting with 2nd element of arr[]...
+    for (i = 1; i < userInput; i++) {
+        temp = arr[i];
+        j = i - 1;
+        while ((temp < arr[j]) && (j >= 0)) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = temp;
+    }
 
-	printf("After sorting: ");
-	for (i = 0; i < userInput; i++)
-		printf(" %d", arr[i]);
-	printf("\n");
+    printf("After sorting: ");
+    for (i = 0; i < userInput; i++) {
+        printf(" %d", arr[i]);
+    }
+    printf("\n");
 
-	// Manage memory.
-	free(arr);
-	arr = NULL;
+    // Manage memory.
+    free(arr);
+    arr = NULL;
 
-	return 0;
+    return 0;
 }
