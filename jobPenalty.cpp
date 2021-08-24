806294174ff0b103bcfd946e96df374b6f6b1595
Rufian55 <chriskearns@rigidbody.tech>
2021-08-24 09:31:29 -0500

    Refreshed

--- jobPenalty.cpp
+++ jobPenalty.cpp
@@ -1,58 +1,66 @@
+/******************************************************************************
+* jobPenalty.cpp - given an array of tasks(penalty, deadlines), minimize the
+* total penalty while maximizing the tasks completed.
+* Compile g++ jobPenalty.cpp -o JobPenalty -g -Wall -std=c++0x
+*******************************************************************************/
 #include<iostream>
 #include<vector>
 
 struct Job {
-	int penalty;
-	int deadline;
-	Job(int penalty, int deadline) : penalty(penalty), deadline(deadline) {};
-	Job() : penalty(0), deadline(0) {};
+    int penalty;
+    int deadline;
+    // Constructor.
+    Job(int penalty, int deadline) : penalty(penalty), deadline(deadline) {};
+    // Default Constructor.
+    Job() : penalty(0), deadline(0) {};
 };
 
-int earliest(bool* timeslot, int onTime, int late) {
-	// look for an on time slot
-	while (onTime >= 0) {
-		if (timeslot[onTime]) {
-			return onTime;
-		}
-		onTime--;
-	}
-	// find latest unused slot
-	while (late >= onTime) {
-		if (timeslot[late]) {
-			return late;
-		}
-		late--;
-	}
-	return -1;
+int earliest(bool* timeSlot, int onTime, int late) {
+    // Search for an onTime slot.
+    while (onTime >= 0) {
+        if (timeSlot[onTime]) {
+            return onTime;
+        }
+        onTime--;
+    }
+    // Find latest unused slot.
+    while (late >= onTime) {
+        if (timeSlot[late]) {
+            return late;
+        }
+        late--;
+    }
+    return -1;
 }
 
 Job* schedule(Job* tasks, int n) {
-	bool timeSlot[n];
-	Job* schedule = new Job[n];
-	int slot;
+    bool timeSlot[n];
+    Job* schedule = new Job[n];
+    int slot;
 
-	// sort the tasks list if needed....
+    // Sort the tasks list here if needed.
 
-	// set up our available timeslots
-	for (int i = 0; i < n; i++) {
-		timeSlot[i] = true;
-	}
+    // Set up our available timeslots
+    for (int i = 0; i < n; i++) {
+        timeSlot[i] = true;
+    }
 
-	// iterate each job, assigning a timeSlot
-	for (int i = 0; i < n; i++) {
-		slot = earliest(timeSlot, tasks[i].deadline - 1, n);  // find slot
-		timeSlot[slot] = false;         // update that the slot is used
-		schedule[slot] = tasks[i];      // add the task to the schedule slot
-	}
-	return schedule;
+    // Iterate each job, assigning a timeSlot.
+    for (int i = 0; i < n; i++) {
+        slot = earliest(timeSlot, tasks[i].deadline - 1, n); // Find slot.
+        timeSlot[slot] = false;                              // Update that the slot is used.
+        schedule[slot] = tasks[i];                           // Add the task to the schedule slot.
+    }
+    return schedule;
 }
 
 int main() {
-	// I'm creating the list sorted by penalty so we can skip the sort code, also 0 indexing it
-	Job tasks[] = { Job(66, 3), Job(20, 2), Job(15,6), Job(14,1), Job(10,7), Job(9,0), Job(5,8), Job(4,4), Job(2,5) };
-	Job* result = schedule(tasks, 9);
-	for (int i = 0; i < 9; i++) {
-		std::cout << i << "-" << i + 1 << ": " << result[i].deadline << ", " << result[i].penalty << std::endl;
-	}
-	//delete[] result;
+    // List sorted by penalty in order to avoid sort and 0 indexing.
+    Job tasks[] = { Job(66, 3), Job(20, 2), Job(15,6), Job(14,1), Job(10,7), Job(9,0), Job(5,8), Job(4,4), Job(2,5) };
+    Job* result = schedule(tasks, 9);
+    for (int i = 0; i < 9; i++) {
+        std::cout << i << "-" << i + 1 << ": " << result[i].deadline << ", " << result[i].penalty << std::endl;
+    }
+
+    delete[] result;
 }
