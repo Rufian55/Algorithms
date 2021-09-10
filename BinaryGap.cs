96aed44cfe259a613304132beb08f51b4149a3e7
Rufian55 <chriskearns@rigidbody.tech>
2021-09-10 15:53:58 -0500

    commented

--- BinaryGap.cs
+++ BinaryGap.cs
@@ -1,11 +1,16 @@
 /*********************************************************************
  * BinaryGap.cs returns longest substring of zeros surrounded by ones.
+ * Runs O(N).
  *********************************************************************/
-public int binaryGap(int N) {
+public int BinaryGap(int N) {
+    // Convert N to a binary string of 0's and 1's.
     string bits = Convert.ToString(N, 2);
-    int length = 0;
-    int bestLength = 0;
-    int tempCount = 0;
+
+    int length = 0;     // Current 0 string length.
+    int bestLength = 0; // Best 0 string length so far.
+    int tempCount = 0;  // Intermediate 0 counter.
+    
+
     for (int i = 0; i < bits.Length; i++) {
         if (bits[i] == '0') {
             if (tempCount > 0) {
@@ -21,5 +26,6 @@ public int binaryGap(int N) {
         if (tempCount > length) length = tempCount;
         if (bits[i] == '1' && length > bestLength) bestLength = length;
     }
+
     return bestLength;
 }
